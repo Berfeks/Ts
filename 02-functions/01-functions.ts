de451ec8a8765df8cdaf5c76055ed73de68bfdfd
Maksym Plevnytskyi <76871115+Berfeks@users.noreply.github.com>
2022-03-13 16:08:16 +0100

    Update 01-functions.ts

--- 02-functions/01-functions.ts
+++ 02-functions/01-functions.ts
@@ -2,9 +2,9 @@
 function add1(x, y) {
     return x + y;
 }
-
 let result1 = add1(1, 2);
 console.log(result1);
+
 
 // типизированная функция. Принимает два аргумента типа number и возвращает значение типа number
 function add2(x: number, y: number) :number {
@@ -12,6 +12,7 @@ function add2(x: number, y: number) :number {
 }
 
 let result2: number = add2(10, 20);
+
 //let result2: number = add2(10); // ожидается два параметра
 //let result2: number = add2(10, "text"); // второй аргумент должен быть числовым
 //let result2: string = add2(10, 20); // результат работы функции не может быть присвоен строковой переменной
